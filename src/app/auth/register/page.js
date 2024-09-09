3228e77ca4c3d0f4d46520aecf4bd56e5eb29fca
Rebert-Azevedo <rebertazevedo2016@gmail.com>
2024-09-09 14:23:32 -0300

    Inclusão de página de cadastro

---
+++ src/app/auth/register/page.js
@@ -0,0 +1,41 @@
+export default function Register() {
+    return (
+        <div className="w-full min-h-screen flex items-center justify-center align-middle bg-slate-900">
+            <div className="box">
+                <form id="register-form" autoComplete="off" method="post">
+                    <h1>Cadastro</h1>
+                    
+                    <div className="inputBox">
+                        <input type="text" required="required" />
+                        <span>Nome Completo</span>
+                        <i></i>
+                    </div>
+                    
+                    <div className="inputBox">
+                        <input type="email" required="required" />
+                        <span>Email</span>
+                        <i></i>
+                    </div>
+
+                    <div className="inputBox">
+                        <input type="text" required="required" />
+                        <span>CPF</span>
+                        <i></i>
+                    </div>
+                    
+                    <div className="inputBox">
+                        <input type="password" required="required" />
+                        <span>Senha</span>
+                        <i></i>
+                    </div>
+
+                    <div className="links">
+                        <a href="#">Já tem uma conta? Login</a>
+                    </div>
+
+                    <input type="submit" value="Cadastrar" />
+                </form>
+            </div>
+        </div>
+    );
+}
