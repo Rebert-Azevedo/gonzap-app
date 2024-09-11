934e5edafd96cd0c7aee18430b01919adb7868dc
Rebert Azevedo <113703728+Rebert-Azevedo@users.noreply.github.com>
2024-09-11 14:13:11 -0300

    update funis

--- src/app/(authenticated)/funis/page.js
+++ src/app/(authenticated)/funis/page.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 
 function GerenciarFunis() {
   const [funis, setFunis] = useState([]);
-  const [novoFuni, setNovoFuni] = useState({ nome: '', tipo: '', arquivo: null });
+  const [novoFunil, setNovoFunil] = useState({ nome: '', tipo: '', arquivo: null });
   const [termoBusca, setTermoBusca] = useState('');
 
   // Simulação de arquivos importados anteriormente
@@ -14,25 +14,25 @@ function GerenciarFunis() {
     { nome: 'Documento 1', tipo: 'documento', arquivo: 'documento1.pdf' },
   ];
 
-  const handleAdicionarFuni = () => {
-    setFunis([...funis, novoFuni]);
-    setNovoFuni({ nome: '', tipo: '', arquivo: null });
+  const handleAdicionarFunil = () => {
+    setFunis([...funis, novoFunil]);
+    setNovoFunil({ nome: '', tipo: '', arquivo: null });
   };
 
-  const handleExcluirFuni = (index) => {
+  const handleExcluirFunil = (index) => {
     const novosFunis = [...funis];
     novosFunis.splice(index, 1);
     setFunis(novosFunis);
   };
 
-  const handleEditarFuni = (index, novoNome, novoTipo, novoArquivo) => {
+  const handleEditarFunil = (index, novoNome, novoTipo, novoArquivo) => {
     const novosFunis = [...funis];
     novosFunis[index] = { nome: novoNome, tipo: novoTipo, arquivo: novoArquivo };
     setFunis(novosFunis);
   };
 
-  const funisFiltrados = funis.filter(funi =>
-    funi.nome.toLowerCase().includes(termoBusca.toLowerCase())
+  const funisFiltrados = funis.filter(funil =>
+    funil.nome.toLowerCase().includes(termoBusca.toLowerCase())
   );
 
   return (
@@ -45,13 +45,13 @@ function GerenciarFunis() {
         <input
           type="text"
           placeholder="Nome do funil"
-          value={novoFuni.nome}
-          onChange={(e) => setNovoFuni({ ...novoFuni, nome: e.target.value })}
+          value={novoFunil.nome}
+          onChange={(e) => setNovoFunil({ ...novoFunil, nome: e.target.value })}
           className="w-full p-3 border text-black border-gray-300 rounded-md mb-4 focus:outline-none focus:border-indigo-500"
         />
         <select
-          value={novoFuni.tipo}
-          onChange={(e) => setNovoFuni({ ...novoFuni, tipo: e.target.value })}
+          value={novoFunil.tipo}
+          onChange={(e) => setNovoFunil({ ...novoFunil, tipo: e.target.value })}
           className="w-full p-3 border text-black border-gray-300 rounded-md mb-4 focus:outline-none focus:border-indigo-500"
         >
           <option value="">Selecione o tipo de arquivo</option>
@@ -61,26 +61,26 @@ function GerenciarFunis() {
           <option value="documento">Documento</option>
         </select>
         <select
-          value={novoFuni.arquivo ? novoFuni.arquivo.nome : ''}
+          value={novoFunil.arquivo ? novoFunil.arquivo.nome : ''}
           onChange={(e) => {
             const arquivoSelecionado = arquivosImportados.find(arquivo => arquivo.nome === e.target.value);
-            setNovoFuni({ ...novoFuni, arquivo: arquivoSelecionado });
+            setNovoFunil({ ...novoFunil, arquivo: arquivoSelecionado });
           }}
           className="w-full p-3 border text-black border-gray-300 rounded-md mb-4 focus:outline-none focus:border-indigo-500"
         >
           <option value="">Selecione o arquivo</option>
-          {arquivosImportados.filter(arquivo => arquivo.tipo === novoFuni.tipo).map((arquivo, index) => (
+          {arquivosImportados.filter(arquivo => arquivo.tipo === novoFunil.tipo).map((arquivo, index) => (
             <option key={index} value={arquivo.nome}>{arquivo.nome}</option>
           ))}
         </select>
 
         <button
-          onClick={handleAdicionarFuni}
-          className={`w-full py-2 rounded-md font-semibold transition-colors ${!novoFuni.nome || !novoFuni.tipo || !novoFuni.arquivo
+          onClick={handleAdicionarFunil}
+          className={`w-full py-2 rounded-md font-semibold transition-colors ${!novoFunil.nome || !novoFunil.tipo || !novoFunil.arquivo
             ? "bg-gray-300 text-gray-500 cursor-not-allowed"
             : "bg-indigo-500 text-white hover:bg-indigo-600"
             }`}
-          disabled={!novoFuni.nome || !novoFuni.tipo || !novoFuni.arquivo} // desabilita o botão se o nome, tipo ou arquivo estiverem vazios
+          disabled={!novoFunil.nome || !novoFunil.tipo || !novoFunil.arquivo} // desabilita o botão se o nome, tipo ou arquivo estiverem vazios
         >
           Adicionar
         </button>
@@ -105,20 +105,20 @@ function GerenciarFunis() {
             </tr>
           </thead>
           <tbody>
-            {funisFiltrados.map((funi, index) => (
+            {funisFiltrados.map((funil, index) => (
               <tr key={index}>
-                <td className="py-3 px-4">{funi.nome}</td>
-                <td className="py-3 px-4">{funi.tipo}</td>
-                <td className="py-3 px-4">{funi.arquivo.nome}</td>
-                <td className="py-3 px-4 text-center">
+                <td className="py-3 px-4 text-black">{funil.nome}</td>
+                <td className="py-3 px-4 text-black">{funil.tipo}</td>
+                <td className="py-3 px-4 text-black">{funil.arquivo.nome}</td>
+                <td className="py-3 px-4 text-black text-center">
                   <button
-                    onClick={() => handleExcluirFuni(index)}
+                    onClick={() => handleExcluirFunil(index)}
                     className="bg-red-500 text-white py-1 px-2 rounded-md hover:bg-red-600"
                   >
                     Excluir
                   </button>
                   <button
-                    onClick={() => handleEditarFuni(index, funi.nome, funi.tipo, funi.arquivo)}
+                    onClick={() => handleEditarFunil(index, funil.nome, funil.tipo, funil.arquivo)}
                     className="bg-yellow-500 text-white py-1 px-2 rounded-md hover:bg-yellow-600 ml-2"
                   >
                     Editar
