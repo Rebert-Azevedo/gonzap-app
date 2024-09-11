68e7c2053f31c4c0953a26eb861e1322843802e6
Rebert Azevedo <113703728+Rebert-Azevedo@users.noreply.github.com>
2024-09-11 14:49:01 -0300

    adição de foco nos botões do dashboard

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -6,14 +6,55 @@ export default function Sidebar() {
             <h1 className="text-2xl font-bold text-orange-500 mb-4">GONZAP</h1>
             <p className="text-sm mb-8">VERSÃO: 1.0.0</p>
             <ul className="space-y-4">
-                <li><button className="w-full bg-orange-500 p-2 text-left rounded"><Link href={'/dashboard'}>Início</Link></button></li>
-                <li><button className="w-full bg-orange-500 p-2 text-left rounded"><Link href={'/mensagens'}>Mensagens</Link></button></li>
-                <li><button className="w-full bg-orange-500 p-2 text-left rounded"><Link href={'/audios'}>Áudios</Link></button></li>
-                <li><button className="w-full bg-orange-500 p-2 text-left rounded"><Link href={'/midias'}>Mídias</Link></button></li>
-                <li><button className="w-full bg-orange-500 p-2 text-left rounded"><Link href={'/documentos'}>Documentos</Link></button></li>
-                <li><button className="w-full bg-orange-500 p-2 text-left rounded"><Link href={'/funis'}>Funis</Link></button></li>
-                <li><button className="w-full bg-orange-500 p-2 text-left rounded"><Link href={'/gatilhos'}>Gatilhos</Link></button></li>
-                <li><button className="w-full bg-orange-500 p-2 text-left rounded"><Link href={'/backups'}>Backups</Link></button></li>
+                <li>
+                    <button 
+                        className="w-full bg-orange-500 p-2 text-left rounded focus:outline-none focus:ring-2 focus:ring-orange-300"
+                    >
+                        <Link href={'/dashboard'}>Início</Link>
+                    </button>
+                </li>
+                <li>
+                    <button 
+                        className="w-full bg-orange-500 p-2 text-left rounded focus:outline-none focus:ring-2 focus:ring-orange-300"
+                    >
+                        <Link href={'/mensagens'}>Mensagens</Link>
+                    </button>
+                </li>
+                <li>
+                    <button 
+                        className="w-full bg-orange-500 p-2 text-left rounded focus:outline-none focus:ring-2 focus:ring-orange-300"
+                    >
+                        <Link href={'/audios'}>Áudios</Link>
+                    </button>
+                </li>
+                <li>
+                    <button 
+                        className="w-full bg-orange-500 p-2 text-left rounded focus:outline-none focus:ring-2 focus:ring-orange-300"
+                    >
+                        <Link href={'/documentos'}>Documentos</Link>
+                    </button>
+                </li>
+                <li>
+                    <button 
+                        className="w-full bg-orange-500 p-2 text-left rounded focus:outline-none focus:ring-2 focus:ring-orange-300"
+                    >
+                        <Link href={'/funis'}>Funis</Link>
+                    </button>
+                </li>
+                <li>
+                    <button 
+                        className="w-full bg-orange-500 p-2 text-left rounded focus:outline-none focus:ring-2 focus:ring-orange-300"
+                    >
+                        <Link href={'/gatilhos'}>Gatilhos</Link>
+                    </button>
+                </li>
+                <li>
+                    <button 
+                        className="w-full bg-orange-500 p-2 text-left rounded focus:outline-none focus:ring-2 focus:ring-orange-300"
+                    >
+                        <Link href={'/backups'}>Backups</Link>
+                    </button>
+                </li>
             </ul>
         </aside>
     )
