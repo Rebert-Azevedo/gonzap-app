e334772ce6660ff8ad484ce15e50e66a290bc222
Rebert Azevedo <113703728+Rebert-Azevedo@users.noreply.github.com>
2024-10-03 15:44:32 -0300

    Atualizações

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,10 @@
 export default function Header() {
     return (
-        <header className="fixed z-10 w-full bg-slate-600 flex justify-between py-2 px-4 items-center shadow-md">
+        <header className="fixed z-10 w-full bg-orange-900 flex justify-between py-2 px-4 items-center shadow-md">
             <h2 className="text-lg">Olá, Rebert!</h2>
             <div className="flex items-center">
                 <p className="mr-4">Dúvidas ao utilizar o painel de controle?</p>
-                <button className="bg-orange-500 text-white px-4 py-2 rounded">Fale com nosso suporte</button>
+                <button className="bg-slate-800 text-white px-4 py-2 rounded">Fale com nosso suporte</button>
             </div>
         </header>
     )
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,63 +1,58 @@
 import Link from "next/link";
-import Image from "next/image";
 
 export default function Sidebar() {
     return (
-        <aside className="min-h-screen w-1/4 bg-slate-800 p-4">
-            <div className="flex items-center mb-4">
-                <img
-                    src="../../public/logo.png" 
-                    width={50}
-                    height={50}
-                />
+        <aside className="min-h-screen w-1/4 bg-orange-500 p-4">
+            <div className="flex justify-center items-center mb-4">
+                <img src="/teste.png" alt="Gonzap" className="w-40 h-40" />
             </div>
-            <p className="text-sm mb-8">VERSÃO: 1.0.0</p>
+            <p className="text-sm mb-10">VERSÃO: 1.0.0</p>
             <ul className="space-y-4">
                 <li>
                     <button
-                        className="w-full bg-orange-500 p-2 text-left rounded focus:outline-none focus:ring-2 focus:ring-orange-300"
+                        className="w-full bg-slate-800 p-2 text-left rounded focus:outline-none focus:ring-2 focus:ring-slate-300"
                     >
                         <Link href={'/dashboard'}>Início</Link>
                     </button>
                 </li>
                 <li>
                     <button
-                        className="w-full bg-orange-500 p-2 text-left rounded focus:outline-none focus:ring-2 focus:ring-orange-300"
+                        className="w-full bg-slate-800 p-2 text-left rounded focus:outline-none focus:ring-2 focus:ring-slate-300"
                     >
                         <Link href={'/mensagens'}>Mensagens</Link>
                     </button>
                 </li>
                 <li>
                     <button
-                        className="w-full bg-orange-500 p-2 text-left rounded focus:outline-none focus:ring-2 focus:ring-orange-300"
+                        className="w-full bg-slate-800 p-2 text-left rounded focus:outline-none focus:ring-2 focus:ring-slate-300"
                     >
                         <Link href={'/audios'}>Áudios</Link>
                     </button>
                 </li>
                 <li>
                     <button
-                        className="w-full bg-orange-500 p-2 text-left rounded focus:outline-none focus:ring-2 focus:ring-orange-300"
+                        className="w-full bg-slate-800 p-2 text-left rounded focus:outline-none focus:ring-2 focus:ring-slate-300"
                     >
                         <Link href={'/documentos'}>Documentos</Link>
                     </button>
                 </li>
                 <li>
                     <button
-                        className="w-full bg-orange-500 p-2 text-left rounded focus:outline-none focus:ring-2 focus:ring-orange-300"
+                        className="w-full bg-slate-800 p-2 text-left rounded focus:outline-none focus:ring-2 focus:ring-slate-300"
                     >
                         <Link href={'/funis'}>Funis</Link>
                     </button>
                 </li>
                 <li>
                     <button
-                        className="w-full bg-orange-500 p-2 text-left rounded focus:outline-none focus:ring-2 focus:ring-orange-300"
+                        className="w-full bg-slate-800 p-2 text-left rounded focus:outline-none focus:ring-2 focus:ring-slate-300"
                     >
                         <Link href={'/gatilhos'}>Gatilhos</Link>
                     </button>
                 </li>
                 <li>
                     <button
-                        className="w-full bg-orange-500 p-2 text-left rounded focus:outline-none focus:ring-2 focus:ring-orange-300"
+                        className="w-full bg-slate-800 p-2 text-left rounded focus:outline-none focus:ring-2 focus:ring-slate-300"
                     >
                         <Link href={'/backups'}>Backups</Link>
                     </button>
