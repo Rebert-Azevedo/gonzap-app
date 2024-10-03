56b2fae0b417493329688c35bafb12c0505865c9
Rebert Azevedo <113703728+Rebert-Azevedo@users.noreply.github.com>
2024-10-03 17:32:50 -0300

    Update Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,10 @@
 export default function Header() {
     return (
-        <header className="fixed z-10 w-full bg-orange-900 flex justify-between py-2 px-4 items-center shadow-md">
+        <header className="fixed z-10 w-full bg-slate-800 flex justify-between py-2 px-4 items-center shadow-md">
             <h2 className="text-lg">Olá, Rebert!</h2>
             <div className="flex items-center">
                 <p className="mr-4">Dúvidas ao utilizar o painel de controle?</p>
-                <button className="bg-slate-800 text-white px-4 py-2 rounded">Fale com nosso suporte</button>
+                <button className="bg-orange-800 text-white px-4 py-2 rounded focus:outline-none focus:ring-2 focus:ring-orange-300">Fale com nosso suporte</button>
             </div>
         </header>
     )
